dfd92d9d9d2177acb3fb22fd961d414c58979756
sip-crypto <sippapat002@gmail.com>
2026-03-24 22:56:16 +0700

    Implement reply to user messages in webhook handler

--- api/webhook.js
+++ api/webhook.js
@@ -2,7 +2,25 @@ export default async function handler(req, res) {
   const events = req.body.events;
 
   if (events && events.length > 0) {
-    console.log("User said:", events[0].message?.text);
+    const replyToken = events[0].replyToken;
+    const userText = events[0].message?.text;
+
+    await fetch("https://api.line.me/v2/bot/message/reply", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer K8tYcnPGzm3c8ReE+dHJH544y5/z//Q76wYHdFHoF2bHS/O4IunpRmyKn8Ms28pf21QLd5ofDiLFiCPosmxhywN9dyPBSpB9kxFpcm8gMz1t77MZY3dUa0eZaP3z1VaLZPHMnAaPNwliYUsnkkwkAAdB04t89/1O/w1cDnyilFU=`
+      },
+      body: JSON.stringify({
+        replyToken: replyToken,
+        messages: [
+          {
+            type: "text",
+            text: `คุณพิมพ์ว่า: ${userText}`
+          }
+        ]
+      })
+    });
   }
 
   res.status(200).send("ok");
