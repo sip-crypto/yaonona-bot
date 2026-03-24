cb722485b1cfca40b28d9ea773600b5073f60b32
sip-crypto <sippapat002@gmail.com>
2026-03-24 23:06:48 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -5,40 +5,13 @@ export default async function handler(req, res) {
     const replyToken = events[0].replyToken;
     const userText = events[0].message?.text;
 
-    await fetch("https://api.line.me/v2/bot/message/reply", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer K8tYcnPGzm3c8ReE+dHJH544y5/z//Q76wYHdFHoF2bHS/O4IunpRmyKn8Ms28pf21QLd5ofDiLFiCPosmxhywN9dyPBSpB9kxFpcm8gMz1t77MZY3dUa0eZaP3z1VaLZPHMnAaPNwliYUsnkkwkAAdB04t89/1O/w1cDnyilFU=`
-      },
-      body: JSON.stringify({
-        replyToken: replyToken,
-        messages: [
-          {
-            type: "text",
-            text: `คุณพิมพ์ว่า: ${userText}`
-          }
-        ]
-      })
-    });
-  }
-
-  res.status(200).send("ok");
-}
-export default async function handler(req, res) {
-  const events = req.body.events;
-
-  if (events && events.length > 0) {
-    const replyToken = events[0].replyToken;
-    const userText = events[0].message?.text;
-
     const aiText = `กำลังวิเคราะห์: ${userText}`;
 
     await fetch("https://api.line.me/v2/bot/message/reply", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer YOUR_ACCESS_TOKEN`
+        "Authorization": `Bearer K8tYcnPGzm3c8ReE+dHJH544y5/z//Q76wYHdFHoF2bHS/O4IunpRmyKn8Ms28pf21QLd5ofDiLFiCPosmxhywN9dyPBSpB9kxFpcm8gMz1t77MZY3dUa0eZaP3z1VaLZPHMnAaPNwliYUsnkkwkAAdB04t89/1O/w1cDnyilFU=`
       },
       body: JSON.stringify({
         replyToken,
